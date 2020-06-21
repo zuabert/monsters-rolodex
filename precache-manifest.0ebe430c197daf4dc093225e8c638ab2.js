d6cbebe3343e89cc911f6492c98e15e771b569ac
Denys Abert <denys_abert@epam.com>
2020-06-21 14:17:43 -0400

    Updates

---
+++ precache-manifest.0ebe430c197daf4dc093225e8c638ab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b701bcac7d1ef808456d5ebeae57793",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c989fcb5e8c213f7cf27",
+    "url": "/monsters-rolodex/static/css/main.718b0517.chunk.css"
+  },
+  {
+    "revision": "59e07d13fedd4a313f37",
+    "url": "/monsters-rolodex/static/js/2.4bb0ac66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.4bb0ac66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c989fcb5e8c213f7cf27",
+    "url": "/monsters-rolodex/static/js/main.3e02183d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
